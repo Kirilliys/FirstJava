f149d556f9723bf7c03a9faf82267ff3d9d51c5b
Kirilliys <Kirilliys12@mail.ru>
2021-11-27 00:01:59 +0300

    создание новой ветки и переменных

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Hello my first script"); // выводит информацию в модальном окне
+const title = 10; // создание переменной
+const screens = 10;
+const screenPrice = 10;
+const rollback = 10;
+const fullPrice = 10;
+const adaptive = 10;
+
+console.log(title);
