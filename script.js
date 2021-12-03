0229ae8571fe9ce660da13bd9406d6085198425f
Kirilliys <Kirilliys12@mail.ru>
2021-12-04 00:18:36 +0300

    create: ознакомился с функциями

--- script.js
+++ script.js
@@ -1,51 +1,73 @@
 
-
 let title = prompt('Как называется ваш проект?');
-
-
 let screens = prompt('Какие типы экранов нужно разработать?','пример: "Простые, Сложные, Интерактивные"');
-
-
 let screenPrice = +prompt('Сколько будет стоить данная работа?', 'пример:1200')
-
-
 let adaptive = confirm('Нужен ли адаптив на сайте?');
-
+let rollback = 56;
 
 let service1 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice1 = +prompt('Сколько это будет стоить?');
 let service2 = prompt('Какой дополнительный тип услуги нужен?');
 let servicePrice2 = +prompt('Сколько это будет стоить?');
 
+const showTypeOf = function(variable){
+  console.log(variable, typeof variable);
+}
 
-let fullPrice = screenPrice + servicePrice1 + servicePrice2
+// Объявление функции типа function expression
+const getAllServicePrices = function(servicePrice1,servicePrice2){
+          return servicePrice1 + servicePrice2
+}
+allServicePrice = getAllServicePrices(servicePrice1,servicePrice2);
+
+// Объявление функции типа function declaration
+function getFullPrice(a,b){
+  return a + b
+}
+fullPrice = getFullPrice(screenPrice,allServicePrice);
 
 
+// Объявление функции с названием (первая большая буква, отсальные маленькие)
+let getTitle = function(title){
+  title = title.trim().toLowerCase();
+  return title[0].toUpperCase() + title.substring(1);
 
-let rollback = 56;
-rollback = rollback / 100;
-rollback = fullPrice * rollback;
+}
 
+// Объявление функции возвращения итоговой стоимости за выетом отката
+let getServicePercentPrices = function(){
+  return Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
+}
+servicePersentPrice = getServicePercentPrices();
 
-let servicePersentPrice = Math.ceil(fullPrice - rollback );
-console.log(servicePersentPrice,'rub');
-
-
-switch (true) {
-  case 30000 <= fullPrice:
-    console.log('Даём скидку в 10%');
+// Объявление функции с условием
+const getRollbackMessage = function(price){
+  switch (true) {
+  case 30000 <= price:
+    return 'Даём скидку в 10%'
     break
-  case ((15000 <= fullPrice) && (fullPrice < 30000)):
-    console.log('Даём скидку в 5%');
+  case ((15000 <= price) && (price< 30000)):
+    return 'Даём скидку в 5%'
     break
-  case( (0 <= fullPrice) && (fullPrice < 15000)):
-    console.log('Скидка не предусмотрена');
+  case( (0 <= price) && (price < 15000)):
+    return 'Скидка не предусмотрена'
     break
   default:
-    console.log('Что-то пошло не так');
-
+    return 'Что-то пошло не так'
+ }
 }
 
 
 
+
+
+
+showTypeOf(title)
+showTypeOf(screenPrice)
+showTypeOf(adaptive)
+
+// превращение строки в массив
+console.log(screens.split(' '));
+console.log(getRollbackMessage(fullPrice));
+console.log(servicePersentPrice,'rub');
 
