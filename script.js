48d77755725f4984c4d6c56ffe40278f7b52ddaf
Kirilliys <Kirilliys12@mail.ru>
2021-12-13 01:32:52 +0300

    fixed: исправил баг с выводом чисел в консоль

--- script.js
+++ script.js
@@ -1,97 +1,56 @@
-let title = prompt('Как называется ваш проект?');
-let screens = prompt('Какие типы экранов нужно разработать?','пример: "Простые, Сложные, Интерактивные"');
-let screenPrice 
-let adaptive = confirm('Нужен ли адаптив на сайте?');
-let rollback = 56;
+'use strict'
 
-let service1 
-let servicePrice1 
-let service2 
-let servicePrice2 
+let a
 
-const showTypeOf = function(variable){
-  console.log(variable, typeof variable);
+
+
+function isNumber(a) {
+  return !isNaN(parseFloat(a)) && isFinite(a) 
 }
 
-const isNumber = function(num) {
-  return !isNaN(parseFloat(num)) && isFinite(num) && !num.trim().length
+function num(x) {
+
+
+
+     function two() {
+
+    a = prompt("Введите число от 0 до 100")
+    if (a === null || a === ' ') {
+       alert('До свидания')
+      return;
+    }
+
+    if(!isNumber(a)) {
+      alert('Необходимо ввести целое число от 0 до 100')
+      two();
+      return;
+    }
+
+
+   else if (a < x) {
+      alert('Необходимо ввести число больше')
+      two(); 
+      return;
+
+
+    } 
+   else if (a > x ) {
+    alert("Необходимо ввести число меньше")
+    two();
+    return;
+    } 
+
+   else if (a === x) {
+      alert("Поздравляем, вы выйграли")
+      return;
+   }
+   console.log(a);
+ 
+  }
+ 
+   console.log(x);
+  two()
 }
 
-const asking = function() {
-  do {
-    let screenPrice = prompt('Сколько будет стоить данная работа?', 'пример:1200')
-  } while(!isNumber(screenPrice))
-}
-
-
-
-// Объявление функции типа function expression
-const getAllServicePrices = function(){
-          let sum = 0
-
-          for (let i = 0; i < 2; i++){
-            if (i === 0){
-              service1 = prompt('Какой дополнительный тип услуги нужен?')
-            } else if (i === 1) {
-              service2 = prompt('Какой дополнительный тип услуги нужен?')
-            }
-            sum += +prompt('Сколько это будет стоить?')
-          }
-
-          return sum
-}
-
-
-// Объявление функции типа function declaration
-function getFullPrice(a,b){
-  return a + b
-}
-
-
-
-// Объявление функции с названием (первая большая буква, отсальные маленькие)
-let getTitle = function(title){
-  title = title.trim().toLowerCase();
-  return title[0].toUpperCase() + title.substring(1);
-
-}
-
-// Объявление функции возвращения итоговой стоимости за выетом отката
-let getServicePercentPrices = function(){
-  return Math.ceil(fullPrice - (fullPrice * (rollback / 100)));
-}
-
-
-// Объявление функции с условием
-const getRollbackMessage = function(price){
-  switch (true) {
-  case 30000 <= price:
-    return 'Даём скидку в 10%'
-    break
-  case ((15000 <= price) && (price< 30000)):
-    return 'Даём скидку в 5%'
-    break
-  case( (0 <= price) && (price < 15000)):
-    return 'Скидка не предусмотрена'
-    break
-  default:
-    return 'Что-то пошло не так'
- }
-}
-
-asking()
-allServicePrice = getAllServicePrices(servicePrice1,servicePrice2);
-fullPrice = getFullPrice(screenPrice,allServicePrice);
-servicePersentPrice = getServicePercentPrices();
-
-
-
-showTypeOf(title)
-showTypeOf(screenPrice)
-showTypeOf(adaptive)
-
-// превращение строки в массив
-console.log(screens.split(' '));
-console.log(getRollbackMessage(fullPrice));
-console.log(servicePersentPrice,'rub');
-
+ 
+ num(5)
